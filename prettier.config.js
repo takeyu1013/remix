d1dc44176c477bac273e1f70af67dbc6fb9c5e85
takeyu1013 <yuto1013@gmail.com>
2023-10-07 13:58:22 +0000

    🔧 Improve auto sort ordering

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,19 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrder: [
+    "<TYPES>^(react/(.*)$)|^(react$)",
+    "<TYPES>^(@remix-run/(.*)$)|^(@remix-run$)",
+    "<TYPES>",
+    "",
+    "^(react/(.*)$)|^(react$)",
+    "^(@remix-run/(.*)$)|^(@remix-run$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "<TYPES>^[~]",
+    "<TYPES>^[.]",
+    "",
+    "^[~]",
+    "^[.]",
+  ],
 };
