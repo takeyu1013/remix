aeb23a18a3967d1ab866ca2175ce8895e6c6cb1e
takeyu1013 <yuto1013@gmail.com>
2023-10-08 07:16:07 +0000

    ✨ Add support for the search

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 
 import { json, redirect } from "@remix-run/node";
 import {
@@ -26,8 +26,10 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStyleHref },
 ];
 
-export const loader = async () => {
-  const contacts = await getContacts();
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return json({ contacts });
 };
 
